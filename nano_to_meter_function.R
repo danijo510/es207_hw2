64b3539e61f946852accae1d0622f0621e2b07bd
Danielle Jones <djones47@ucmerced.edu>
2020-02-06 19:15:11 -0800

    Nanometer to meter function

---
+++ nano_to_meter_function.R
@@ -0,0 +1,9 @@
+nano_to_meter <- function(nanometer)
+{
+  
+  meter <- nanometer/1000000000 #Converting nanometer to meters by dividing by 10^9
+  meter #Calling the meter function
+  
+}
+
+nano_to_meter(40000000)
